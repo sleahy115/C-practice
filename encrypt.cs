63b68aef8c11f20ae1f0312b51fe89e7f04adf60
sleahy115 <sleahy115@gmail.com>
2018-03-25 00:24:48 -0600

    encryption 2

--- encrypt.cs
+++ encrypt.cs
@@ -7,8 +7,8 @@ class Encrypt
 	{
 		Console.WriteLine("Enter a phrase to encrypt");
 		string phrase = Console.ReadLine();
-		string encryptedPhrase = phrase.Replace("e", "o");
-		string encryptedPhrase2 = encryptedPhrase.Replace("a", "1");
-		Console.WriteLine("Your encrypted phrase is: " + encryptedPhrase2);
+		phrase = phrase.ToLower().Replace("a","1").Replace("e", "2").Replace("i", "3")
+		.Replace("o", "4").Replace("u", "5").Replace("y", "!");
+		Console.WriteLine("Your encrypted phrase is: " + phrase);
 	}
 }
