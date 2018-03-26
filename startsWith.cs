60c1fb68f4b3eb5538628acdeacb945b4e078f17
sleahy115 <sleahy115@gmail.com>
2018-03-26 14:47:16 -0600

    mutiple conditions

--- startsWith.cs
+++ startsWith.cs
@@ -12,6 +12,10 @@ class startsWith
 		{
 			Console.WriteLine("Like Zorro you are awesome!");
 		}
+		else if(userName.StartsWith("S"))
+		{
+			Console.WriteLine("Awesome! You have the best first inital!!");
+		}
 		else
 		{
 			Console.WriteLine("You are awesome, but your name will never be cool like Zorro :(");
