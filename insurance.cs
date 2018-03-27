8be4358e91bc760919d650bd98ff8b6e23095570
sleahy115 <sleahy115@gmail.com>
2018-03-26 21:13:18 -0600

    insurance first pass

---
+++ insurance.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Insurance
+{
+	static void Main()
+	{
+		Console.WriteLine("How old are you?");
+		string ageString = Console.ReadLine(); 
+		int age = int.Parse(ageString);
+		Console.WriteLine("What sex are you? M/F");
+		string sex = Console.ReadLine();
+		int quote = 50;
+		if(age < 25 && sex == "M")
+		{
+			quote = quote * 4;
+			Console.WriteLine("Your quote for auto insurance is" + quote);
+		}
+		if(age < 25)
+		{
+			quote = quote * 2;
+			Console.WriteLine("Your quote for auto insurance is" + quote);
+		}
+		else if(sex == "M")
+		{
+			quote = quote * 2;
+			Console.WriteLine("Your quote for auto insurance is" + quote);
+		}
+		else
+		{
+			Console.WriteLine("Your quote for auto insurance is" + quote);
+		}
+
+	}
+}
