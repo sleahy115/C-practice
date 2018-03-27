c09b744bd0b4ce0ca137b60c6085ab381dd55594
sleahy115 <sleahy115@gmail.com>
2018-03-26 21:17:38 -0600

    insurance second pass

--- insurance.cs
+++ insurance.cs
@@ -9,25 +9,26 @@ class Insurance
 		int age = int.Parse(ageString);
 		Console.WriteLine("What sex are you? M/F");
 		string sex = Console.ReadLine();
+		sex = sex.ToLower();
 		int quote = 50;
-		if(age < 25 && sex == "M")
+		if(age < 25 && sex == "m")
 		{
 			quote = quote * 4;
-			Console.WriteLine("Your quote for auto insurance is" + quote);
+			Console.WriteLine("Your quote for auto insurance is " + quote);
 		}
-		if(age < 25)
+		else if(age < 25)
 		{
 			quote = quote * 2;
-			Console.WriteLine("Your quote for auto insurance is" + quote);
+			Console.WriteLine("Your quote for auto insurance is " + quote);
 		}
 		else if(sex == "M")
 		{
 			quote = quote * 2;
-			Console.WriteLine("Your quote for auto insurance is" + quote);
+			Console.WriteLine("Your quote for auto insurance is " + quote);
 		}
 		else
 		{
-			Console.WriteLine("Your quote for auto insurance is" + quote);
+			Console.WriteLine("Your quote for auto insurance is " + quote);
 		}
 
 	}
