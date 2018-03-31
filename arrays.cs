432c6395c55c8deb7d4aba8d53a140d3fb960496
sleahy115 <sleahy115@gmail.com>
2018-03-31 16:30:05 -0600

    mpre array practice

--- arrays.cs
+++ arrays.cs
@@ -5,10 +5,15 @@ class Array
 {
 	static void Main()
 	{
-		string[] fruits = {"apple", "bannana", "grapefruit"};
+		string[] fruits = {"apple", "grape", "grapefruit"};
+		string[] badFruits = {"Bannana", "blueberry", "durien"};
 		int[] numbers = {3, 6, 9, 1};
+		string replaced = fruits[2].Replace(fruits[2], badFruits[2]);
+		int multiplied = numbers[1] * 5;
 
 		Console.WriteLine("My favorite fruit is " + fruits[2]);
 		Console.WriteLine("My favorite number is " + numbers[1]);
+		Console.WriteLine("My least favorite fruit is " + replaced);
+		Console.WriteLine("My favorite number multiplied by 5 is " + multiplied);
 	}
 }
