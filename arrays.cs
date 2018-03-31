c2ff87403758ddec3211216a8b1b4c337138ece6
sleahy115 <sleahy115@gmail.com>
2018-03-31 16:10:32 -0600

    array practice

---
+++ arrays.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Array
+
+{
+	static void Main()
+	{
+		string[] fruits = {"apple", "bannana", "grapefruit"};
+		int[] numbers = {3, 6, 9, 1};
+
+		Console.WriteLine("My favorite fruit is " + fruits[2]);
+		Console.WriteLine("My favorite number is " + numbers[1]);
+	}
+}
