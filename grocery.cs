3cec467b5411e00ae274f977acd34b7e39c61c36
sleahy115 <sleahy115@gmail.com>
2018-04-02 01:31:22 -0600

    Add grocery price

--- grocery.cs
+++ grocery.cs
@@ -6,10 +6,19 @@ class GroceryList
 	static void Main()
 	{
 		string[] myGroceries = {"eggs", "milk", "fruit"};
+		int[] groceryPrices = {3,5, 10};
+		int total = 0;
 		Console.WriteLine("Your grocery list is:");
 		foreach(string groceryItem in myGroceries)
 		{
 			Console.WriteLine(groceryItem);
 		} 
+		foreach(int price in groceryPrices)
+		{
+			total= total + price;
+		}
+
+		Console.WriteLine("Your groceries will cost " + total);
+
 	}
 }
