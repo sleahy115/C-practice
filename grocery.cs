5412765c3a2c77c94d01108a1c918751317811e2
sleahy115 <sleahy115@gmail.com>
2018-04-02 11:44:57 -0600

    for loop

--- grocery.cs
+++ grocery.cs
@@ -5,20 +5,28 @@ class GroceryList
 {
 	static void Main()
 	{
-		string[] myGroceries = {"eggs", "milk", "fruit"};
-		int[] groceryPrices = {3,5,10};
-		int total = 0;
-		Console.WriteLine("Your grocery list is:");
-		foreach(string groceryItem in myGroceries)
-		{
-			Console.WriteLine(groceryItem);
-		} 
-		foreach(int price in groceryPrices)
-		{
-			total = total + price;
-		}
+		// string[] myGroceries = {"eggs", "milk", "fruit"};
+		// int[] groceryPrices = {3,5,10};
+		// int total = 0;
+		// Console.WriteLine("Your grocery list is:");
+		// foreach(string groceryItem in myGroceries)
+		// {
+		// 	Console.WriteLine(groceryItem);
+		// } 
+		// foreach(int price in groceryPrices)
+		// {
+		// 	total = total + price;
+		// }
 
-		Console.WriteLine("Your groceries will cost " + total);
+		// Console.WriteLine("Your groceries will cost " + total);
+		string [] groceryList = {"eggs", "milk", "bread", "toothpaste"};
+
+		Console.WriteLine("My grocery list:");
+
+		for(int index = 0; index < groceryList.Length; index++)
+		{
+			Console.WriteLine(index + 1 + " " + groceryList[index]);
+		}
 
 	}
 }
