b1eec5dd668d208a7a5d93d098edec27d85d1378
sleahy115 <sleahy115@gmail.com>
2018-04-23 21:32:28 -0600

    car objects

---
+++ car.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+Class Car
+{
+	public string Make;
+	public string Model;
+	public int Year;
+	public int Mileage;
+}
+
+Public class Program
+{
+	public static void Main()
+	{
+		Car Ford = new Car();
+		Ford.Make = Ford;
+		Ford.Model = Tarus;
+		Ford.Year = 2016;
+		Ford.Mileage = 10000;
+
+		Car Mazda = new Car();
+		Mazda.Make = Mazda;
+		Madza.Model = 3;
+		Mazda.Year = 2018;
+		Mazda.Mileage = 100;
+
+		Car Porsche = new Car;
+		Porsche.Make = Porsche;
+		Porsche.Model = Cayenne;
+		Porsche.Year = 2017;
+		Porsche.Mileage = 3000;
+
+		List<Car> = new List<Car>() { Ford, Mazda, Porsche };
+	}
+}
