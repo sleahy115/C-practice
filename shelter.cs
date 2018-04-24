eafca0f9ab14f6de738a74869a4458b7b908e049
sleahy115 <sleahy115@gmail.com>
2018-04-23 22:30:27 -0600

    animal shelter first pass

---
+++ shelter.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections. Generic;
+
+public class Animal
+{
+	public string Type;
+	public string Breed;
+	public string Name;
+	public int Age;
+	public bool Nutered;
+}
+
+class Program
+{
+	public void static Main()
+	{
+		Animal Dog = new Animal();
+		Dog.Type = "Dog";
+		Dog.Breed = "Mutt";
+		Dog.Name = "Rusty";
+		Dog.Age = 5;
+		Dog.Nutered = Y;
+
+		Animal Cat = new Animal();
+		Cat.Type = "Cat";
+		Cat.Breed = "Siamese";
+		Cat.Name = "King";
+		Cat.Age = 2;
+		Cat.Nutered = N;
+
+		Animal Cat = new Animal();
+		Cat.Type = "Cat";
+		Cat.Breed = "Egyptian";
+		Cat.Name = "Queen";
+		Cat.Age = 1;
+		Cat.Nutered = N;
+
+	}
+}	
