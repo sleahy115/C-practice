5e4739011dcbba434d7dc6d018856e1aa20ff39e
sleahy115 <sleahy115@gmail.com>
2018-04-24 23:18:36 -0600

    search by price and mileage

--- car.cs
+++ car.cs
@@ -7,6 +7,7 @@ public class Car
 	public string Model;
 	public int Year;
 	public int Mileage;
+	public int Price;
 }
 
 public class Program
@@ -18,18 +19,21 @@ public class Program
 		Ford.Model = "Tarus";
 		Ford.Year = 2016;
 		Ford.Mileage = 10000;
+		Ford.Price = 10000;
 
 		Car Mazda = new Car();
 		Mazda.Make = "Mazda";
 		Mazda.Model = "3";
 		Mazda.Year = 2018;
 		Mazda.Mileage = 100;
+		Mazda.Price = 20000;
 
 		Car Porsche = new Car();
 		Porsche.Make = "Porsche";
 		Porsche.Model = "Cayenne";
 		Porsche.Year = 2017;
 		Porsche.Mileage = 3000;
+		Porsche.Price = 35000;
 
 		List<Car> Car = new List<Car>() { Ford, Mazda, Porsche };
 		foreach(Car auto in Car)
@@ -40,18 +44,23 @@ public class Program
 		string maxMilesString = Console.ReadLine();
 		int maxMiles = int.Parse(maxMilesString);
 
+		Console.WriteLine("Enter maximum price");
+		string maxPriceString = Console.ReadLine();
+		int maxPrice = int.Parse(maxPriceString);
+
 		List<Car> CarsMatching = new List<Car>();
 		foreach(Car auto in Car)
 		{
-			if (auto.Mileage < maxMiles)
+			if (auto.Mileage < maxMiles && auto.Price < maxPrice)
 			{
 				CarsMatching.Add(auto);
 			} 	
 		}	
 		foreach(Car vehicle in CarsMatching)
 		{
-			Console.WriteLine(vehicle.Make);
-			Console.WriteLine(vehicle.Mileage);
+			Console.WriteLine("The vehicle make is " + vehicle.Make);
+			Console.WriteLine("The vehicle mileage is " + vehicle.Mileage);
+			Console.WriteLine("The vehicle price is " + vehicle.Price);
 		}
 	}
 }
