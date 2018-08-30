d4f1f3dd4fac429dc72489e1a4fe7e1ec5743059
sleahy115 <sleahy115@gmail.com>
2018-08-29 21:37:32 -0600

    make number a global variable

--- higherOrLower.cs
+++ higherOrLower.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 class HigherOrLower
 {
+		public static readonly Random random = new Random();
+		public static int number = random.Next(-500,500);
 
 	static void Main()
 	{
-		Random random = new Random();
-		int number = random.Next(-500,500);
 		Console.WriteLine(number);
 		Console.WriteLine("Lets play a guessing game. I am thinking of a number between -500 and 500, Can you guess it? I will let you know if you are too high or too low.");
 		string userGuessString = Console.ReadLine();
