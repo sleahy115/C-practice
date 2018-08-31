c851f469177a5cfa9e015f2f66aceefcfa723509
sleahy115 <sleahy115@gmail.com>
2018-08-30 21:28:50 -0600

    create calendar files for testing

--- higherOrLower.cs
+++ higherOrLower.cs
@@ -10,7 +10,6 @@ class HigherOrLower
 	static void Main()
 	{
 		number = random.Next(-500,500); 
-		Console.WriteLine(number);
 		Console.WriteLine("Lets play a guessing game. I am thinking of a number between -500 and 500, Can you guess it? I will let you know if you are too high or too low.");
 			string userGuessString = Console.ReadLine();
 			int userGuess = int.Parse(userGuessString);
@@ -20,7 +19,6 @@ class HigherOrLower
 
 	static void Check(int userGuess)
 	{			
-		Console.WriteLine("Check called");
 		if(userGuess < -500 || userGuess > 500)
 		{
 			Console.WriteLine("Sorry your number is outside of the number range. Please choose a number that is between -500 and 500"); 
@@ -33,7 +31,6 @@ class HigherOrLower
 		
 	static void HighLow(int userGuess)
 	{
-		Console.WriteLine("HighLow called");
 		if(userGuess == number)
 		{
 			Console.WriteLine("Amazing you guessed it. The number I was thinking of was " + number + ".Would you like to play again?y/n" );
