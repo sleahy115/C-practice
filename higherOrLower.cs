bafeac5d3bc3fb5a91bd8da371ef61234c47fe8a
sleahy115 <sleahy115@gmail.com>
2018-08-30 21:10:02 -0600

    playable game with methods

--- higherOrLower.cs
+++ higherOrLower.cs
@@ -9,6 +9,7 @@ class HigherOrLower
 
 	static void Main()
 	{
+		number = random.Next(-500,500); 
 		Console.WriteLine(number);
 		Console.WriteLine("Lets play a guessing game. I am thinking of a number between -500 and 500, Can you guess it? I will let you know if you are too high or too low.");
 			string userGuessString = Console.ReadLine();
