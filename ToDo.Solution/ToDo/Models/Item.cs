bbdddb093529c475b67387a6f84443d558fb31c3
sleahy115 <sleahy115@gmail.com>
2018-09-06 03:46:11 -0600

    create item class

---
+++ ToDo.Solution/ToDo/Models/Item.cs
@@ -0,0 +1,17 @@
+namespace ToDo.Models
+{
+	public class Items
+	{
+		private string _description;
+
+		public Item (string description)
+		{
+			// _description = description;
+		}
+
+		public string GetDescription()
+		{
+			return _description;
+		}
+	}
+}
