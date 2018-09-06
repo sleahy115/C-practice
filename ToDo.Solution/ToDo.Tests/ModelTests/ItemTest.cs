b116367039a118fa78359d9f899c94a4c5ce6111
sleahy115 <sleahy115@gmail.com>
2018-09-06 04:48:33 -0600

    create first test

---
+++ ToDo.Solution/ToDo.Tests/ModelTests/ItemTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDo.Models;
+
+namespace ToDo.Tests
+{
+	[TestClass]
+	public class ItemTest
+	{
+		[TestMethod]
+		public void GetDescription_ReturnsDescription_String()
+		{			
+		//Arrange
+		string description = "Walk dog";
+		Item newItem = new Item(description);
+
+		//Act
+		string result = newItem.GetDescription();
+
+		//Assert
+		Assert.AreEqual(description, result);
+		}
+
+	}
+}
