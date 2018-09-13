036e255ef9a05089ea6a5c768e2ef8ecebae509e
sleahy115 <sleahy115@gmail.com>
2018-09-13 03:03:38 -0600

    create save and get all methods

--- ToDo.Solution/ToDo/Models/Item.cs
+++ ToDo.Solution/ToDo/Models/Item.cs
@@ -1,17 +1,34 @@
+using System.Collections.Generic;
+
 namespace ToDo.Models
 {
 	public class Item
 	{
 		private string _description;
+		private List<Item> _instances = new List<Item> {};
 
 		public Item (string description)
 		{
-			// _description = description;
+			_description = description;
 		}
 
 		public string GetDescription()
 		{
 			return _description;
+		}
+
+		public void SetDescription(newDescription)
+		{
+			_description = newDescription;
+		}
+
+		public static List<Item> GetAll()
+		{
+			return _instances;
+		} 
+		public void Save()
+		{
+			//_instances.Add(this);
 		}
 	}
 }
