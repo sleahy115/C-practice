9336ba8dba754b526d01aa3e0b9ea47bdacd6854
sleahy115 <sleahy115@gmail.com>
2018-09-16 13:17:11 -0600

    create cube class and volume test

---
+++ shapes.solution/rectangle/Models/cube.cs
@@ -0,0 +1,16 @@
+namespace shapes.models
+{
+	public class Cube
+	{
+		private Rectangle_face;
+		public Cube (Rectangle side)
+		{
+			_face = side;
+		}
+
+		public int GetVolume()
+		{
+			return 0;
+		}
+	}
+}
---
+++ shapes.solution/shapes.tests/modelTests/cubeTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using shapes.Models;
+
+namespace Shapes.TestTools
+{
+	[TestClass]
+	public class CubeTests
+	{
+		[TestMethod]
+		public void Cube_GetVolume_VoulmeOfCube()
+		{
+			//Arrange
+			Rectangle newRectangle = new Rectangle(4,4,);
+			Cube newCube = new Cube(newRectangle);
+
+			//Act
+			int result = newCube.GetVolume();
+
+			//Assert
+			Assert.AreEqual(result, 64);
+		}
+	}
+}
